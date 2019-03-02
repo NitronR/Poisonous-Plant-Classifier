75fc5e47eb34e19db02a6445c3efca05d1e76296
NitronR <bhanur05@gmail.com>
2019-03-02 23:05:09 +0530

    readme, styling, about, descriptions

--- README.md
+++ README.md
@@ -1,20 +1,16 @@
 # Poisonous-Plant-Classifier
-A web app that classifies an image into 8 categories of plants.
-
-Took help from [this repository](https://github.com/shankarj67/Water-classifier-fastai) on Water classification.
-
-Kaggle kernel. Used resnet18 model in deployment due to limitations of Heroku free.
-
-Classification better with resnet50.
-
-Used FastAI. Explain
-
 Poisonous plants are dangerous for unsuspecting individuals such as children. Especially the plants that are common in backyards. There are many casualties due to lack of supervision of gardens in public and private areas.
-One solution to this problem is image classification using deep learning. Keeping this in mind I made a custom data set of the poisonous plants that are common in our home area. I got a list of these from here:
-https://www.goodhousekeeping.com/home/gardening/advice/g1174/deadly-poisonous-plants/
-I have made the data set available on Kaggle:
-https://lnkd.in/f4fQCEw
-Using fastai, I have successfully created an Image classifier for classifying a plant's image into 8 categories of poisonous plants. The classifier have 98% accuracy on the current data set. Here is the Kaggle kernel:
-https://lnkd.in/f9bDuRr
-This is one of the first step towards the solution. Next I will make a web app for demonstrating the capabilities of the current model.
+
+One solution to this problem is image classification using deep learning. Keeping this in mind I made a custom data set of the poisonous plants that are common in our home area. I got a list of these from [here](https://www.goodhousekeeping.com/home/gardening/advice/g1174/deadly-poisonous-plants/).
+
+I have made the data set available [on Kaggle](https://www.kaggle.com/nitron/poisonous-plants-images).
+
+Using fastai, I have successfully created an Image classifier for classifying a plant's image into 8 categories of poisonous plants. Create a Convolutional Neural Network and improved the model through interpretation as well as fine tuning. The classifier have 97% accuracy on the current test set. [Here](https://www.kaggle.com/nitron/poisonous-plant-classifier) is the Kaggle kernel.
+
+This repository is for the web app that classifies an image into 8 categories of plants using a resnet18 model.
+
+I took help from [this repository](https://github.com/shankarj67/Water-classifier-fastai) on Water classification that provides the boiler plate code to deploy a fastai model on heroku.
+
+I loaded weights from the Kaggle kernel into the web app. I used resnet18 model in deployment due to limitations of Heroku free. Resnet50 gave better results on Kaggle.
+
 Any helpful tips are welcome :)
--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,14 @@
+let plantInfos = {
+    'lilies': "While they're most popular around Easter, lilies in the Hemerocallis genus endanger cats even after spring. That includes common tiger and daylily varieties. Eating just a small amount can lead to acute kidney failure or death.",
+    'rhubarb': "The garden vegetable's stems do make for delicious pies. Just don't try using leaves. Eating too much will shut down the kidneys, occasionally proving fatal.",
+    'oleander': 'Eating only a small part of this plant (which flowers in white, pink, or red) can be fatal. In one case, just one leaf was enough to harm a child. Symptoms of poisoning include drowsiness, slowed heart rate, and shaking.',
+    'castor_oil_plant': 'Castor oil plays a part in many home remedies, but the actual plant itself is more likely to kill you than cure you. The seeds contain one of the most poisonous naturally occuring substances known to man, according to Cornell University College of Agriculture and Life Sciences. Just one seed can kill a child, as the toxic protein ricin can cause severe dehydration from vomiting and diarrhea.',
+    'dieffenbachia': 'Also known as dumb cane and elephant ear can become deadly if ingested, causing the airways to swell shut. Even brushing against it can cause burning or itching.',
+    'foxglove': "Don't let the pretty colors fool you. These bell-shaped blooms and their berries entice kids, but contain a compound used for treating heart failure. Eating them is like \"taking an unregulated dose of heart medicine,\" according to Poison Control.",
+    'wisteria': 'Thankfully this climbing vine rarely harms humans, but the seed pods are toxic to dogs, cats, and horses. Head to the vet if you develop symptoms like vomiting or diarrhea.',
+    'lily_of_the_valley': 'Breaking Bad fans will remember that Walter White used this sweet-looking plant for some not-so-sweet deeds. Consuming its toxic compounds — called cardiac glycosides — can send people to the hospital with symptoms like dizziness, vomiting, rashes, and diarrhea. If left untreated, those little bells can even cause death.'
+}
+
 $(document).ready(function () {
     // Init
     $('.image-section').hide();
@@ -48,6 +59,7 @@ $(document).ready(function () {
                 if (data.prob > 0.8) {
                     $('#result').html('<b>Prediction:</b>  ' + formatName(data.result));
                     $('#result').append('<br/><b>Surity:</b>  ' + round(data.prob * 100) + '%');
+                    $('#result').append('<br/><b>Description:</b>  ' + plantInfos[data.result]);
                 } else {
                     $('#result').text('Not sure what this is.');
                 }
